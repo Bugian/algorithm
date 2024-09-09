175db752a79b5e9e9e83cf00ea16fec4bd5e2f96
Corin <145905988+Bugian@users.noreply.github.com>
2024-09-09 12:10:51 +0300

    Update C++_longest_Common_Prefix.cpp

--- C++/C++_longest_Common_Prefix.cpp
+++ C++/C++_longest_Common_Prefix.cpp
@@ -1,55 +1,28 @@
-//{ Driver Code Starts
-//Initial template for C++
-
 #include<bits/stdc++.h>
 using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
 
 string longestCommonPrefix (string arr[], int N)
     {
         // your code here
-        string test1 = arr[0], test2;
-        string rez = "";
-        string rasp;
-        for(int i = 0; i < N-1; i++){
-            test2 = arr[i+1];
-            for(int j = 0; j < test1.length(); j++){
-                if(test1[j] == test2[j]){
-                    rez += test1[j];
-                } else{
-                    break;
-                }
-            }
-            if(rasp != "" && rez.length() > rasp.length()){
-                rez = rasp;
-            }
-            rasp = rez;
-            rez = "";
-            if(rasp == ""){
-                rasp = "-1";
-                break;
-            }
-        }
-        
-        if(N == 1){
-            rasp = arr[0];
-        }
+        if(N == 0) return "-1";
+        string prefix = arr[0];
+        for(int i = 1; i < N; i++){
+            string current = arr[i];
+            int j = 0;
             
-        return rasp;
+            while (j < prefix.size() && j < current.size() && 
+                    prefix[i] == current[i]){
+                        j++;
+                    }
+            prefix = prefix.substr(0, j);
+        }
+        return prefix;
     }
 
-
-//{ Driver Code Starts.
 int main()
 {
     
         int n = 5;
-        string arr[5] = {"abcdef", "abcdef", "abcdefg", "ab", "abc"};
+        string arr[5] = {"abcdwef", "abcdwef", "abcdwefg", "abcdw", "abcdww"};
         cout << longestCommonPrefix (arr, n);
 }
-
-// Contributed By: Pranay Bansal
-
-// } Driver Code Ends
